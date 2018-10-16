81af0a5243d62461e8b14fe934876956f8fe1e05
David Wong <davidwong@Davids-MacBook-Pro-3.local>
2018-10-16 19:54:34 -0400

    added few css changes

--- public/app.js
+++ public/app.js
@@ -113,12 +113,5 @@ $(".clear-all").on("click", function () {
 });
 
 $(document).on("click", ".refresh", function () {
-  // Grab the articles as a json
-  $.getJSON("/articles", function (data) {
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</span><span class=delete>X</span></p>");
-    }
-  });
+
 })
