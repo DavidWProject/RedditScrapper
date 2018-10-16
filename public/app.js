553991c64c2d0350352901bcc650dbc6b0ff1b91
David Wong <davidwong@Davids-MacBook-Pro-3.local>
2018-10-16 19:43:41 -0400

    added interactive links

--- public/app.js
+++ public/app.js
@@ -3,7 +3,12 @@ $.getJSON("/articles", function (data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<button class=delete>X</button></p>");
+    if (data[i].link.substring(0,3) === "/r/") {
+      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br /><a href=https://old.reddit.com" + data[i].link + " target=_blank>https://old.reddit.com" + data[i].link + "<button class=delete>X</button></p>");
+    } else {
+      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br /><a href=" + data[i].link + " target=_blank>" + data[i].link + "</a><button class=delete>X</button></p>");
+
+    }
   }
 });
 
