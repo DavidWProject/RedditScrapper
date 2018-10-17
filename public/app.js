8415b09563b26008ae9d825bfb307830517cf05b
David Wong <davidwong@Davids-MacBook-Pro-3.local>
2018-10-16 20:14:47 -0400

    added refresh, post css clean up

--- public/app.js
+++ public/app.js
@@ -30,7 +30,7 @@ $(document).on("click", "p", function () {
     .then(function (data) {
       console.log(data);
       // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
+      $("#notes").append("<h3>" + data.title + "</h3>");
       // An input to enter a new title
       $("#notes").append("<input id='titleinput' name='title' >");
       // A textarea to add a new note body
