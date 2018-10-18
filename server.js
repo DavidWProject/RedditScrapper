d9e86a4bf3c82beccd879ed81bf99a82bd471931
David Wong <davidwong@Davids-MacBook-Pro-3.local>
2018-10-18 13:11:04 -0400

    deploying database to heroku via mongolab

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ var db = require("./models");
 
 var PORT = 3000;
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
 // Initialize Express
 var app = express();
 
@@ -29,7 +31,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true
 });
 
